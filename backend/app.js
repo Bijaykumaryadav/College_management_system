0b6b35bb6a4b47b7199ea9d23115a338497b845a
bijay kumar yadav <ybijayyadav468@gmail.com>
2024-07-19 18:47:19 +0530

    fixing bug for deploy

--- backend/app.js
+++ backend/app.js
@@ -67,9 +67,9 @@ dbConnection();
 
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../frontend")));
+  app.use(express.static(path.join(__dirname1, "../frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "../frontend", "index.html"));
+    res.sendFile(path.resolve(__dirname1, "../frontend", "dist", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
