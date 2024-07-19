205748f051de19c2aa96334d3a490952f6c0fc60
bijay kumar yadav <ybijayyadav468@gmail.com>
2024-07-19 16:50:27 +0530

    production ready cod

--- backend/app.js
+++ backend/app.js
@@ -66,7 +66,7 @@ dbConnection();
 /*---------------------DEPLOYMENT-----------------------*/
 
 const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "PRODUCTION") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "../frontend/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname1, "../frontend", "dist", "index.html"));
