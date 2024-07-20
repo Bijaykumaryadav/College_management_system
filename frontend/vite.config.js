0414b0311d7d5bbaf5783b56508687d699b0a72b
bijay kumar yadav <ybijayyadav468@gmail.com>
2024-07-20 22:46:06 +0530

    fixing frontend bugs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // This is the default, change if needed
+  },
+  chunkSizeWarningLimit: 1000, // Adjust this value to avoid chunk size warnings
 });
